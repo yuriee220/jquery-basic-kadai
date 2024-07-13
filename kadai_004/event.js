325c7fb82d6a4a98bd5632ea22d668406b00ed1f
yuriee220 <y.date220@gmail.com>
2024-07-13 14:54:58 +0900

    kadai_004をアップロード

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+$(window).on('load', () => {
+        console.log('loadイベントが発生しました');
+});
+
+$(function() {
+    $(window).on('scroll', () => {
+        console.log('scrollイベントが発生しました');
+    });
+});
+    
+
+
